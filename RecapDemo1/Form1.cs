57810e6b53ed652764f716aa34e1f74c83bcd1d8
Berra Özyalvaç <140336658+berraylvc@users.noreply.github.com>
2024-03-28 15:32:36 +0300

    Add files via upload

---
+++ RecapDemo1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecapDemo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateButton();
+        }
+
+        private void GenerateButton()
+        {
+            //64 tane butonu oluşturabilmek için ayrı ayrı 64 tane yazmak yerine for döngüsü kullanırsak hepsini üst üste aynı yere veriyor. Butonların ayrı ayrı ulaşılabilirliği sıkıntı
+            //kullanacağımız yöntem array oluşturma
+            //buton nesneleri birer class'tır
+            Button[,] buttons = new Button[8, 8];
+
+            int top = 0;
+            int left = 0;
+            //konum ayarlaması için tanımlandı 
+
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button(); //her seferinde bize yeni bir buton oluşturacağını ve bu yüzden new'lememiz gerektiğini unutmamamız gerekiyor.
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    left += 50;
+
+
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+
+                    this.Controls.Add(buttons[i, j]);
+                    //buraya kadar yine üst üste oluşturur o yüzden konumunu değiştirmemiz lazım
+                }
+                left = 0; //8 tane oluşturunca en sola (başa) geri dönsün
+                top += 50; //sıra değiştirsin düşeyde
+            }
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intro();
+
+            string sentence = "My name is Ratatoille";
+            var result = sentence.Length;
+            var result2 = sentence.Clone();  //ilgili string değerin bir referansını daha oluşturmaya yarar
+            Console.WriteLine(result2);
+            sentence = "My name is Aang"; //sentence değişti ama result2 değişmediği için üstteki Console bşr önceki cümleyi yazar
+
+            bool result3 = sentence.EndsWith("e"); //sonucun bool şeklinde döndürüldüğüne dikkat
+            bool result4 = sentence.StartsWith("My");
+            var result5 = sentence.IndexOf("name"); //bulamadığında -1 döndürür
+            var result6 = sentence.IndexOf(" "); //ilk bulduğunda çalışmayı bırakacak ve baştan başlıyor
+            var result7 = sentence.LastIndexOf(" "); //aramaya sondan başlar ama index numaralandırması değişmez karıştırma
+            var result8 = sentence.Insert(0, "Hello, "); //bir ifadeye başka bir metni eklemek için kullanılır
+            var result9 = sentence.Substring(3); //metni parçalamak için kullanılır
+            var result10 = sentence.ToLower(); //hepsini küçük karakterlere çevirir
+            var result11 = sentence.ToUpper(); //hepsini büyük karakterlere çevrir
+            var result12 = sentence.Replace(" ","-"); //bir metin içerisinde belli karakterleri değiştirmek için kullanılır
+            var result13 = sentence.Remove(3,4); //metinde belirli bir indexten sonrasını atmak için kullanılır
+
+
+
+            Console.WriteLine(result13);
+
+
+            Console.ReadLine();
+
+        }
+
+        private static void Intro()
+        {
+            string city = "Ankara";
+            string city2 = "İstanbul";
+
+            string result = city + city2;
+
+            Console.WriteLine(city[0]);
+
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(result);
+            Console.WriteLine(String.Format("{0} {1}", city, city2));
+            //3. bir değişken atamamıza gerek kalmadığı için bunu kullanmak daha iyi
+        }
+    }
+}
