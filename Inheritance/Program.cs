78c30565b2256614195d74b7965f2703ff90962f
Berra Özyalvaç <140336658+berraylvc@users.noreply.github.com>
2024-03-28 15:26:57 +0300

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer{FirstName="Engin"}, new Student{FirstName="Derin"}, new Person{FirstName="Ali"}
+            };
+            foreach(var  person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Person  //interfaceler tek başına bir anlam ifade etmiyordu ama classlarda hem tek başına hem de inheritance verdiği noktada anlam ifade ediyor.
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Customer : Person  //interfacelerden farklı olan bir diğer özelliği de; interfacelerde birden fazla kez kullanılabiliyorken, inheritance'ler sadece bir kez kulanılabilir.
+    { //hem inheritance hem de interface kullanılacaksa önce inheritance sonra interface/interfaceler yazılır.
+        public string City {get;set;}
+    }
+    class Student: Person
+    {
+        public string Department { get; set; }
+    }
+}
+
+//özellikle abstract classlarda inheritance çok önemli yer teşkil ediyor fakat bir proojede genel olarak interface kullanabiliyorsan interface kullan 
---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+
+namespace InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    class Manager : IWorker, IEat, ISalary  //bir class birden fazla interface'i implemente edebilir
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
